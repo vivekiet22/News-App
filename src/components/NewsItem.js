5213684c91923666a85395b41ddff2be2aaddfb3
vivekiet22 <vivekmaddheshiya205@gmail.com>
2022-07-27 14:38:17 +0530

    finalised

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,36 +1,30 @@
-import { getByTitle } from "@testing-library/react";
-import React, { Component } from "react";
+import React from 'react'
 
-export class NewsItem extends Component {
-  render() {
-    let { title, description, imageUrl, newsUrl } = this.props;
-    return (
-      <div className="my-3">
-        <div className="card" style={{ width: "18rem" }}>
-          <img
-            src={
-              !imageUrl
-                ? "https://c.ndtvimg.com/2022-07/putk8ito_image_625x300_24_July_22.jpg"
-                : imageUrl
-            }
-            className="card-img-top"
-            alt="..."
-          />
-          <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a
-              href={newsUrl}
-              target="_blank"
-              className="btn btn-sm btn-primary"
-            >
-              Read More
-            </a>
-          </div>
-        </div>
-      </div>
-    );
-  }
+const NewsItem = (props)=> {
+        let { title, description, imageUrl, newsUrl, author, date, source } = props;
+        return (
+            <div className="my-3">
+                <div className="card">
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        position: 'absolute',
+                        right: '0'
+                    }
+                    }> 
+                        <span className="badge rounded-pill bg-danger"> {source} </span>
+                    </div>
+                    <img src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top" alt="..." />
+                    <div className="card-body">
+                        <h5 className="card-title">{title}  </h5>
+                        <p className="card-text">{description}</p>
+                        <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+     
 }
 
-export default NewsItem;
+export default NewsItem
